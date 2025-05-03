09ca591c658336ae43f9f6afe217fa1773b4f289
Akhil Yadav <akhilyadav.com@gmail.com>
2025-05-03 11:20:44 -0400

    Wrap Setup file in class to fix Zeitwerk error

--- app/services/setup.rb
+++ app/services/setup.rb
@@ -1,9 +1,11 @@
-require 'cgi'
-require 'debug' if Rails.env.development?
-require 'dotenv'
-require 'google_search_results'
-Dotenv.load
+class Setup
+  require 'cgi'
+  require 'debug' if Rails.env.development?
+  require 'dotenv'
+  require 'google_search_results'
+  Dotenv.load
 
-Dir.glob(File.join(__dir__, 'engine/**/*.rb')).each do |file|
-  require_relative file
+  Dir.glob(File.join(__dir__, 'engine/**/*.rb')).each do |file|
+    require_relative file
+  end
 end
